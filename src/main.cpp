b6e26c0639f3e0122dc04cd828499edad6137081
Quentin Henriet <quentin.henriet@free.fr>
2014-05-08 10:28:58 +0200

    MainWindow added. Just shows a KTextEdit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <KApplication>
 #include <KAboutData>
 #include <KCmdLineArgs>
-#include <KMessageBox>
 #include <KLocale>
+
+#include "mainwindow.h"
 
 int main (int argc, char *argv[])
 {
@@ -12,18 +13,16 @@ int main (int argc, char *argv[])
                           "0.0.1",
                           ki18n("Tracks your time"),
                           KAboutData::License_GPL,
-                          ki18n("Quentin Henriet © 2014"),
+                          ki18n("Copyright © 2014 Quentin Henriet"),
                           ki18n("Some text..."), // no text in the About box for the moment
                           0, // No website for the moment
                           "quentin.henriet@free.fr" );
- 
     KCmdLineArgs::init( argc, argv, &aboutData );
+
     KApplication app;
-    KGuiItem yesButton( i18n( "Hello" ), QString(),
-                        i18n( "This is a tooltip" ),
-                        i18n( "This is a WhatsThis help text." ) );
-    return 
-        KMessageBox ::questionYesNo 
-        (0, i18n( "Hello World" ), i18n( "Hello" ), yesButton ) 
-        == KMessageBox ::Yes? EXIT_SUCCESS: EXIT_FAILURE;
+
+    MainWindow* window = new MainWindow();
+    window->show();
+
+    return app.exec();
 }
