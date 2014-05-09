ac770efb851ea59f6e68588b22d2b269da8337ad
Quentin Henriet <quentin.henriet@free.fr>
2014-05-09 22:14:54 +0200

    NewTaskDialog and NewProjectDialog better designed.

--- src/storage.cpp
+++ src/storage.cpp
@@ -20,7 +20,7 @@ QTreeWidgetItem* Storage::addProject(QString& name)
     Task* newProject = new Task();
     m_tasks.insert(newProject->m_uid, newProject);
     newProject->m_widgetItem->setText(0, name);
-    newProject->m_widgetItem->setText(1, "00:00");
+    newProject->m_widgetItem->setText(1, "00:00:00");
     newProject->m_name = name;
     return newProject->m_widgetItem;
 }
@@ -30,7 +30,7 @@ QTreeWidgetItem* Storage::addTask(QString& project, Task* parent, QString& name)
     Task* newTask = new Task();
     m_tasks.insert(newTask->m_uid, newTask);
     newTask->m_widgetItem->setText(0, name);
-    newTask->m_widgetItem->setText(1, "00:00");
+    newTask->m_widgetItem->setText(1, "00:00:00");
     newTask->m_name = name;
     newTask->m_project = project;
     newTask->m_parent = parent;
